5998beae0b1bcef91ab331a40f90f10bc56705c5
Lukasz <lukasz.zatyka@gmail.com>
2024-02-27 21:30:09 +0100

    Markdown and code snippets customization

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import expressiveCode from "astro-expressive-code";
+import { expressiveCodeOptions } from "./src/site.config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    expressiveCode(expressiveCodeOptions),
     icon(),
     tailwind({
     applyBaseStyles: false,
