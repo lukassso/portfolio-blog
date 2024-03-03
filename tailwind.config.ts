06dc26220156b25e258ef16ad71c449486693182
Lukasz <lukasz.zatyka@gmail.com>
2024-03-03 11:29:33 +0100

    Reorganized config styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,23 @@ export default {
 		fontVariantNumeric: false,
 	},
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+      },
+      textColor: {
+        default: "var(--color-text)",
+        offset: "var(--color-text-offset)",
+      },
+      backgroundColor: {
+        default: "var(--color-background)",
+        offset: "var(--color-background-offset)",
+      },
+      borderColor: {
+        default: "var(--color-border)",
+      },
+		},
 	},
 	plugins: [
 		require("@tailwindcss/typography")],
