e4bbcdc4ef92ecb6b8558e6addc2f7497c747f83
Lukasz <lukasz.zatyka@gmail.com>
2024-06-25 12:53:47 +0200

    Refactored Firebase configuration for environment-based

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 
+const prefix = import.meta.env.DEV ? "PUBLIC_" : "";
+
 const firebaseConfig = {
-	apiKey: import.meta.env.FIREBASE_API_KEY,
-	authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.FIREBASE_APP_ID,
-	measurementId: import.meta.env.MEASUREMENT_ID,
+	apiKey: import.meta.env[`${prefix}FIREBASE_API_KEY`],
+	authDomain: import.meta.env[`${prefix}FIREBASE_AUTH_DOMAIN`],
+	projectId: import.meta.env[`${prefix}FIREBASE_PROJECT_ID`],
+	storageBucket: import.meta.env[`${prefix}FIREBASE_STORAGE_BUCKET`],
+	messagingSenderId: import.meta.env[`${prefix}FIREBASE_MESSAGING_SENDER_ID`],
+	appId: import.meta.env[`${prefix}FIREBASE_APP_ID`],
+	measurementId: import.meta.env[`${prefix}MEASUREMENT_ID`],
 };
 
 export const app = initializeApp(firebaseConfig);
