07c7b25523805121016262dc1ae68b4c7815f56b
Lukasz <lukasz.zatyka@gmail.com>
2024-06-25 16:43:56 +0200

    Added authentication for third party apps

---
+++ src/pages/api/auth/third-party-signin.ts
@@ -0,0 +1,32 @@
+import type { APIRoute } from "astro";
+import { app } from "../../../firebase/server";
+import { getAuth } from "firebase-admin/auth";
+
+export const POST: APIRoute = async ({ request, cookies, redirect }) => {
+	const auth = getAuth(app);
+	const { idToken } = await request.json();
+
+	if (!idToken) {
+		return new Response("No token found", { status: 401 });
+	}
+
+	/* Verify id token */
+	try {
+		const decodedToken = await auth.verifyIdToken(idToken);
+		const user = await auth.getUser(decodedToken.uid);
+
+		/* Create and set session cookie */
+		const fiveDays = 60 * 60 * 24 * 5 * 1000;
+		const sessionCookie = await auth.createSessionCookie(idToken, {
+			expiresIn: fiveDays,
+		});
+
+		cookies.set("__session", sessionCookie, {
+			path: "/",
+		});
+
+		return redirect("/dashboard");
+	} catch (error) {
+		return new Response("Invalid token", { status: 401 });
+	}
+};
